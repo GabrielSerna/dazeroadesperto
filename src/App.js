d7b2bbd7bb5320b8640dd5ae6f7ca9aec07382b7
capocolo <gabrielserna.hce@gmail.com>
2023-02-09 18:01:52 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Navbar';
+import About from './About.js';
+import Contact from './Contact.js';
+import Home from './Home.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const app = () => {
   return (
-    <div></div>
+    <Router>
+      <div>
+        < Navbar />
+          <Routes>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/home" element={<Home/>}></Route>
+            <Route path="/contact" element={<Contact/>}></Route>
+          </Routes>
+
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Sono nella pagina di Contact</div>
+  )
+}
+
+export default Contact
