c7b5201320e07a3e2767b1484281395f1fd7d678
capocolo <gabrielserna.hce@gmail.com>
2023-02-13 17:54:14 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -9,13 +9,12 @@ const app = () => {
   return (
     <Router>
       <div>
-        < Navbar />
-          <Routes>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/home" element={<Home/>}></Route>
-            <Route path="/contact" element={<Contact/>}></Route>
-          </Routes>
-
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+        </Routes>
       </div>
     </Router>
   )
