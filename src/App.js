fc15f222652c63848f66d84710fd95517be5c6e5
capocolo <gabrielserna.hce@gmail.com>
2023-02-14 14:22:32 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './Navbar';
 import About from './About.js';
 import Contact from './Contact.js';
+import Details from './Details';
+import ErrorPage from './ErrorPage.js';
 import Home from './Home.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -11,9 +13,11 @@ const app = () => {
       <div>
         <Navbar/>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/*" element={<ErrorPage />}></Route>
         </Routes>
       </div>
     </Router>
