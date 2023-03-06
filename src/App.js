9d2d84f71143f54c1a30678319fa7f90d4ed9a1b
capocolo <gabrielserna.hce@gmail.com>
2023-03-06 16:51:32 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Search from './components/Search';
-import MovieList from './components/MovieList';
 
 const app = () => {
   return (
     <div>
-      <Search />
-      <MovieList />
+
     </div>
   );
 };
